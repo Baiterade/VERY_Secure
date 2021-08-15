482c35155fd51a8646886907d0e34b49d4f9ca97
Jager O <44758792+Baiterade@users.noreply.github.com>
2021-08-14 19:37:40 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Prompt for all parameters and write password to the #password input
+// Prompt for all parameters and write password to the #password text area
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
 
 }
@@ -14,46 +15,71 @@ generateBtn.addEventListener("click", writePassword);
 
 // Runs through prompts
 function generatePassword() {
-  var charSelection = "";
   var newpassword = "";
+
+  let lowercaseString = "qwertyuioplkjhgfdsazxcvbnm";
+  let uppercaseString = "QWERTYUIOPLKJHGFDSAZXCVBNM";
+  let numericString = "0123456789";
+  let specCharString = "!@#*+=<>?:";
+
+  const lowercaseArray = lowercaseString.split("");
+  const uppercaseArray = uppercaseString.split("");
+  const numericArray = numericString.split("");
+  const specCharArray = specCharString.split("");
 
   //Asks for desired length
   var length = prompt("How long should the password be?", " (8-128)");
 
   //Asks for desired character types
-  var lowercase = prompt("Would you like to use lowercase?", "")
-  var uppercase = prompt("Would you like to use uppercase?", "")
-  var numeric = prompt("Would you like to use numbers?", "")
-  var specChar = prompt("Would you like to use special characters?", "")
+  var lowercaseP = confirm("Would you like to use lowercase letters?");
+  console.log(lowercaseP);
+  var uppercaseP = confirm("Would you like to use uppercase letters?");
+  console.log(uppercaseP);
+  var numericP = confirm("Would you like to use numbers?");
+  console.log(numericP);
+  var specCharP = confirm("Would you like to use special characters?");
+  console.log(specCharP);
 
-  if(lowercase == "yes" || lowercase == "Yes"){
-    charSelection += "qwertyuioplkjhgfdsazxcvbnm";
-  }
+  if ((lowercaseP || uppercaseP || numericP || specCharP) && (length >= 8 && length <= 128)) {
 
-  if(uppercase == "yes" || uppercase == "Yes"){
-    charSelection += "QWERTYUIOPLKJHGFDSAZXCVBNM";
-  }
+    while(newpassword.length < length){
+      if (lowercaseP){
+        newpassword += lowercaseArray[Math.round(Math.random()*lowercaseArray.length)]
+        if (newpassword.length == length) {
+          break;
+        }
+      }
 
-  if(numeric == "yes" || numeric == "Yes"){
-    charSelection += "1234567890";
-  }
+      if (uppercaseP){
+        newpassword += uppercaseArray[Math.round(Math.random()*uppercaseArray.length)]
+        if (newpassword.length == length) {
+          break;
+        }
+      }
 
-  if(specChar == "yes" || specChar == "Yes"){
-    charSelection += "!@#$%^&*-_+=,.<>/?:";
+      if (numericP){
+        newpassword += numericArray[Math.round(Math.random()*numericArray.length)]
+        if (newpassword.length == length) {
+          break;
+        }
+      }
+
+      if (specCharP){
+        newpassword += specCharArray[Math.round(Math.random()*specCharArray.length)]
+        if (newpassword.length == length) {
+          break;
+        }
+      }
+
+      console.log(newpassword);
+      }
+  } else {
+    confirm("Error: Please select a length between 8 and 128, and at least one criteria.");
   }
   
-  //Final set of characters to be used
-  console.log(charSelection);
-
-  //Array of usable characters
-  var charSelection = charSelection.split("");
-
-  console.log(charSelection);
-
-  //Picks random characters from charSelection, until the desired length is met
-  for (var i = 0; i < length; i++){
-    newpassword += charSelection[Math.round(Math.random()*charSelection.length)]
-  }
+  var shuffleArray = newpassword.split("");
+  shuffleArray.sort(() => Math.random() - 0.5);
+  newpassword = shuffleArray.join("");
 
   //Created password
   console.log(newpassword);
